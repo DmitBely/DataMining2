8a881e6ddd8b90a6d40cebb109a7f9579cc8bbe4
dmitriy <dmitbely@protonmail.ch>
2021-03-12 17:30:34 +0300

    Lesson 5 HW

--- gb_parse/loaders.py
+++ gb_parse/loaders.py
@@ -1,10 +1,13 @@
+from scrapy.loader import ItemLoader
+from urllib.parse import urljoin
 from scrapy import Selector
-from scrapy.loader import  ItemLoader
 from itemloaders.processors import TakeFirst, MapCompose
+from .items import GbAutoYoulaItem
 
-def get_characteritics(item) -> dict:
+
+def get_characteristics(item):
     selector = Selector(text=item)
-    return {
+    data = {
         "name": selector.xpath(
             "//div[contains(@class, 'AdvertSpecs_label')]/text()"
         ).extract_first(),
@@ -12,13 +15,44 @@ def get_characteritics(item) -> dict:
             "//div[contains(@class, 'AdvertSpecs_data')]//text()"
         ).extract_first(),
     }
-
-def some(data:dict):
-    data["new_key"] = "hello"
     return data
+
+
+def create_user_url(user_id):
+    return urljoin("https://youla.ru/user/", user_id)
+
+
+def clear_price(price: str) -> float:
+    try:
+        return float(price.replace("\u2009", ""))
+    except ValueError:
+        return float("NaN")
+
 
 class AutoyoulaLoader(ItemLoader):
     default_item_class = dict
-    # url_out = TakeFirst()
-    # title_out = TakeFirst()
-    characteristics_in = MapCompose(get_characteritics, some)
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    characteristics_in = MapCompose(get_characteristics)
+    author_in = MapCompose(create_user_url)
+    author_out = TakeFirst()
+    price_in = MapCompose(clear_price)
+    price_out = TakeFirst()
+    descriptions_out = TakeFirst()
+
+
+def flat_text(items):
+    return "\n".join(items)
+
+
+def hh_user_url(user_id):
+    return urljoin("https://hh.ru/", user_id)
+
+
+class HHLoader(ItemLoader):
+    default_item_class = dict
+    url_out = TakeFirst()
+    title_out = TakeFirst()
+    salary_out = flat_text
+    author_in = MapCompose(hh_user_url)
+    author_out = TakeFirst()
